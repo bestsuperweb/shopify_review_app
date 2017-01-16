2d1f24eb64935e5ad9f196350b7e2f0d517ff924
Riley <r-zammit@hotmail.com>
2017-01-15 20:56:17 -0500

    first commit

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ReviewRad
+  class Application < Rails::Application
+    config.action_dispatch.default_headers['P3P'] = 'CP="Not used"'
+    config.action_dispatch.default_headers.delete('X-Frame-Options')
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+
+  config.api_key = "8cc60c6f102d71de76ba84d45697cf0b"
+  config.secret = "202e340a3010e48805a24f2af084e63b"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
